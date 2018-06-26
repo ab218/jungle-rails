942e0a8550e4f337ffdf2dd8717373361dad602b
A B <ab218@live.com>
2018-06-26 13:41:12 -0700

    testing productpage

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: :poltergeist do
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
   def open_asset(file_name)
     File.open(Rails.root.join('db', 'seed_assets', file_name))
   end
-  
+
     # SETUP
     before :each do
       @category = Category.create! name: 'Apparel'
 
+      10.times do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -19,6 +20,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: :poltergeist
         price: 64.99
       )
       end
+    end
 
 
   scenario "They see all products" do
